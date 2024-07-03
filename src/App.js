bd8be45f49ee4034e1d6cb90fe0a122614080ab5
Doina <leovchind@gmail.com>
2024-07-03 19:04:01 +0300

    Added lazy loading and react-router-dom v6 for route components in App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React, { lazy, Suspense } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./styles/App.css";
+import "./styles/index.css";
 
-function App() {
+// Lazy-loaded components
+const PersonalInfo = lazy(() => import("./components/PersonalInfo"));
+const SelectPlan = lazy(() => import("./components/SelectPlan"));
+const AddOns = lazy(() => import("./components/AddOns"));
+const Summary = lazy(() => import("./components/Summary"));
+const Confirmation = lazy(() => import("./components/Confirmation"));
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header"></header>
-    </div>
+    <Router>
+      <div className="app">
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path="/" element={<PersonalInfo />} />
+            <Route path="/select-plan" element={<SelectPlan />} />
+            <Route path="/add-ons" element={<AddOns />} />
+            <Route path="/summary" element={<Summary />} />
+            <Route path="/confirmation" element={<Confirmation />} />
+          </Routes>
+        </Suspense>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
