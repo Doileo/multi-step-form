fbaa77a18041e1e8020569d10c827862a0c15a10
Doina <leovchind@gmail.com>
2024-07-19 19:35:44 +0300

    Applied styles to the PersonalInfo component for mobile view. Still need to resolve the issue with the navigation buttons' placement.

--- src/components/PersonalInfo/PersonalInfo.js
+++ src/components/PersonalInfo/PersonalInfo.js
@@ -13,31 +13,33 @@ const PersonalInfo = ({ onNextStep }) => {
 
   return (
     <StepLayout currentStep={1}>
-      {/* Content */}
-      <div className="info-content">
-        <h1 className="info-heading">Personal Info</h1>
-        <p className="info-text">
-          Please provide your name, email address, and phone number.
-        </p>
+      <div className="personal-info-container">
+        {/* Content */}
+        <div className="info-content">
+          <h1 className="info-heading">Personal info</h1>
+          <p className="info-text">
+            Please provide your name, email address, and phone number.
+          </p>
 
-        <form className="info-form">
-          <div className="form-group">
-            <label htmlFor="name">Name</label>
-            <input type="text" id="name" placeholder="e.g. Stephen King" />
-          </div>
-          <div className="form-group">
-            <label htmlFor="email">Email Address</label>
-            <input
-              type="email"
-              id="email"
-              placeholder="e.g. stephenking@lorem.com"
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="phone">Phone Number</label>
-            <input type="tel" id="phone" placeholder="e.g. +1 234 567 890" />
-          </div>
-        </form>
+          <form className="info-form">
+            <div className="form-group">
+              <label htmlFor="name">Name</label>
+              <input type="text" id="name" placeholder="e.g. Stephen King" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="email">Email Address</label>
+              <input
+                type="email"
+                id="email"
+                placeholder="e.g. stephenking@lorem.com"
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="phone">Phone Number</label>
+              <input type="tel" id="phone" placeholder="e.g. +1 234 567 890" />
+            </div>
+          </form>
+        </div>
 
         {/* Navigation Button */}
         <div className="navigation-buttons">
